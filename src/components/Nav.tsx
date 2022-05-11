b1be3abf9619dff4436974a94ac83bffe8c5fc5e
Frederik Reiter <kontakt@frederikreiter.de>
2022-05-11 15:41:53 +0200

    Fix navbar

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,20 +1,66 @@
-import { Container, AppBar, Toolbar, IconButton, Typography } from '@mui/material';
+import { Container, AppBar, Toolbar, IconButton, Typography, Button, Box, Menu, MenuItem } from '@mui/material';
 import MenuIcon from '@mui/icons-material/Menu';
 import { Link } from 'react-router-dom';
+import React from 'react';
 
 const Nav = () => {
+    const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(null);
+
+    const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
+        setAnchorElNav(event.currentTarget);
+    };
+    const handleCloseNavMenu = () => {
+        setAnchorElNav(null);
+    };
+
     return <AppBar position="static">
         <Container maxWidth="xl">
             <Toolbar variant="dense">
-                <IconButton edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
-                    <MenuIcon />
-                </IconButton>
-                <Typography variant="h6" color="inherit" component="div">
+                <Typography variant="h6" color="inherit" component="div" sx={{ flexGrow: 1 }}>
                     DHBW Vorlesungsplan
                 </Typography>
-                <Link to="/">Home</Link>
-                <Link to="/login">Login</Link>
-                <Link to="/settings">Settings</Link>
+                <Box sx={{ display: { xs: 'none', sm: 'flex' } }}>
+                    <Button color="inherit" component={Link} to="/">
+                        Home
+                    </Button>
+                    <Button color="inherit" component={Link} to="/login">
+                        Login
+                    </Button>
+                    <Button color="inherit" component={Link} to="/settings" >
+                        Settings
+                    </Button>
+                </Box>
+                <IconButton edge="start" color="inherit" aria-label="menu" onClick={handleOpenNavMenu} sx={{ display: { xs: 'flex', sm: 'none' } }}>
+                    <MenuIcon />
+                </IconButton>
+                <Menu
+                    id="menu-appbar"
+                    anchorEl={anchorElNav}
+                    anchorOrigin={{
+                        vertical: 'bottom',
+                        horizontal: 'left',
+                    }}
+                    keepMounted
+                    transformOrigin={{
+                        vertical: 'top',
+                        horizontal: 'left',
+                    }}
+                    open={Boolean(anchorElNav)}
+                    onClose={handleCloseNavMenu}
+                    sx={{
+                        display: { xs: 'block', md: 'none' },
+                    }}
+                >
+                    <MenuItem component={Link} to="/">
+                        <Typography textAlign="center">Home</Typography>
+                    </MenuItem>
+                    <MenuItem component={Link} to="/login">
+                        <Typography textAlign="center">Login</Typography>
+                    </MenuItem>
+                    <MenuItem component={Link} to="/settings" >
+                        <Typography textAlign="center">Settings</Typography>
+                    </MenuItem>
+                </Menu>
             </Toolbar>
         </Container>
     </AppBar>
