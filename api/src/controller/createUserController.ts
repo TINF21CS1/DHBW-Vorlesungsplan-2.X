d811c1a1af1954d30fc44a47e3717949e11d2a55
jlg <admin@gruber.ninja>
2022-05-15 23:55:37 +0200

    tsoa

--- api/src/controller/createUserController.ts
+++ api/src/controller/createUserController.ts
@@ -1,16 +1,20 @@
 import {Get, Route} from "tsoa";
-import { Response } from "express";
+import { Response, Request } from "express";
 import {omit} from "lodash";
 import { UserService } from "../service/createUserService";
 
 @Route("/user")
 export class UserController {
     @Get("/")
-    public async createUser(res: Response): Promise<Response> {
+    public async createUser(req: Request, res: Response): Promise<Response> {
         //TODO: auth?
-        const userService = new UserService();
-        const user = await userService.createUser();
-        //convert to json.
-        return res.send(omit);
+        try {
+            const userService = new UserService();
+            const user = await userService.createUser(req.body);
+            return res.send(omit(user.toJSON(), "test"));
+        }catch(e) {
+            return res.status(500);
+        }
+
     }
 }
--- api/src/service/createUserService.ts
+++ api/src/service/createUserService.ts
@@ -2,15 +2,12 @@ import {IUser} from "../models/IUser"
 import {prisma} from "../app"
 
 class UserService {
-    public async createUser(): Promise<IUser> {
+    public async createUser(body: any): Promise<IUser> {
         const user = await prisma.user.create({
             data:{
                 name:"",
                 email:"",
                 salt:"",
-                role:"",
-                hasedpassword:"",
-                pepper:""
             }
         })
         return user
