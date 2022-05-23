c89fdcd3cffceafabd1732cf7ea59f264486964c
Frederik Reiter <kontakt@frederikreiter.de>
2022-05-23 15:35:07 +0200

    Fix tests

--- front/src/App.test.tsx
+++ front/src/App.test.tsx
@@ -4,12 +4,12 @@ import App from './App';
 
 test('renders first date box', () => {
   render(<App />);
-  const e = screen.getByText(/Turing/i);
-  expect(e).toBeInTheDocument();
+  const e = screen.queryAllByText(/Turing/i);
+  expect(e).not.toHaveLength(0);
 });
 
 test('does not show Tutorium', () => {
   render(<App />);
-  const e = screen.getByText(/210B/i);
-  expect(e).not.toBeInTheDocument();
+  const e = screen.queryAllByText('210B')
+  expect(e).toHaveLength(0);
 })
