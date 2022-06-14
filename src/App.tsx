2f29067d682c09d58773549c23dafcd94acfe96e
Frederik Reiter <kontakt@frederikreiter.de>
2022-06-14 19:15:22 +0200

    Remove dependency on Google Fonts

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,10 @@ import {
   Navigate,
 } from "react-router-dom";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 
 const theme = createTheme({
   palette: {
