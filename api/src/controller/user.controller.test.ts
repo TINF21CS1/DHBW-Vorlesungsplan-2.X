0ee787cc716c674d032c8ca4eb3b4c4807d97ffc
J-jlg <66569721+J-jlg@users.noreply.github.com>
2022-06-15 15:27:25 +0200

    Update user.controller.test.ts

--- api/src/controller/user.controller.test.ts
+++ api/src/controller/user.controller.test.ts
@@ -1,19 +1,22 @@
 import UserController from "./user.controller";
+import bcrypt from "bcrypt";
 
-interface User {
-  id: string;
-  name: string;
-  email: string;
-  pass: string;
-  notification: boolean;
-  courseId: string;
-}
+describe('User Tests', () => {
+  const mockUser = {
+    id: "1337", //Id will be discarded anyways.
+    name: 'Jon Doe',
+    email: 'jon@doe.com',
+    pass: 'notsosecret1234',
+    notification: true,
+    courseId: "62a831cca565c6763002fc95",
+  }
+  it("Create a new user. (should return an JWT if successfull)", async () => {
+    const controller = new UserController();
+    await expect(controller.createUser(mockUser)).resolves.toContain("ey")
+  })
 
-test("Testing Usercreation", async () => {
-  const controller = new UserController();
-  let usr: User = JSON.parse(
-    '{ "name": "Jon Doe", "email": "jon@doe.com", "pass": "notsosecret1234", "notification": "true", "selectedCourses": "null" }'
-  );
-  const response = await controller.createUser(usr);
-  expect(response).toBe("Already Exists");
-});
+  it("Test Password-Hashing and Salting-Process", async () => {
+    const pass = bcrypt.hashSync(mockUser.pass, bcrypt.genSaltSync(10));
+    await expect(bcrypt.compare(mockUser.pass, pass))
+  })
+})
