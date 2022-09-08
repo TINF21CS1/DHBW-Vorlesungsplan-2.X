1c0a4b71711f6483a6252b1f88bd49631adbc45c
Frederik Reiter <kontakt@frederikreiter.de>
2022-09-08 19:14:19 +0200

    Read version from package.json
    Remove metaprogramming from GitHub Action

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,11 +1,12 @@
 import { Typography } from "@mui/material";
+import packageJson from "../../package.json";
 
 export default function Footer() {
   return (
     <Typography align="center" component="footer" color="text.secondary">
       Made by frereit on{" "}
-      <a href="https://github.com/frereit/react-calendar/">GitHub</a>{" "}
-      %buildversion%
+      <a href="https://github.com/frereit/react-calendar/">GitHub</a> (v
+      {packageJson.version})
     </Typography>
   );
 }
